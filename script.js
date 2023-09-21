22133ce08471e83263c8ed452bd92a651d25a81a
Lewmatic <mwl2.2000@gmail.com>
2023-09-20 23:11:51 -0500

    Added drawing functionality on the grid
     - Introduced 'handleMouseOver()' to apply the '.black-square' class to
       the event target when 'isMouseDown' is set to true.
     - Incorporated event listeners on the grid to toggle the 'isMouseDown'
       state based on 'mousedown' and 'mouseup' events.
     - Added event listeners to each square within the grid, enabling
       'handleMouseOver()' execution when 'isMouseDown' is true.

--- script.js
+++ script.js
@@ -3,10 +3,32 @@ const grid = document.getElementById("grid");
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const square = document.createElement("div");
-    square.classList.add("grid-squares");
+    square.classList.add("grid-square");
     grid.appendChild(square);
   }
   const clearDiv = document.createElement("div");
   clearDiv.classList.add("clear");
   grid.appendChild(clearDiv);
 }
+
+let isMouseDown = false;
+
+function handleMouseOver(event) {
+  if (isMouseDown) {
+    event.target.classList.add("black-square");
+  }
+}
+
+const squares = grid.querySelectorAll(".grid-square");
+
+grid.addEventListener("mousedown", () => {
+  isMouseDown = true;
+});
+
+grid.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
+
+squares.forEach((square) => {
+  square.addEventListener("mousemove", handleMouseOver);
+});
